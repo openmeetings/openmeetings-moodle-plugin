b7be085b31df2b81c9d693ec9123c8a6179c612d
michaelwuttke1058 <michaelwuttke1058@gmail.com>
2014-05-12 20:46:44 +0000

    plugin works also with moodle 2.2 & 2.3; $module changed in $plugin

--- version.php
+++ version.php
@@ -20,11 +20,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version    = 2014031603;
-$module->component  = 'mod_openmeetings';
-$module->maturity   = MATURITY_STABLE;
-$module->requires   = 2012120309;
-$module->release    = '1.7';
-$module->cron       = 0;
-$module->om_version = '3.0+';
+$plugin->version    = 2014031604;
+$plugin->component  = 'mod_openmeetings';
+$plugin->maturity   = MATURITY_STABLE;
+$plugin->requires   = 2011120511;
+$plugin->release    = '1.7';
+$plugin->cron       = 0;
+$plugin->om_version = '3.0+';
 
